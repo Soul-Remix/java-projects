e5f9fc7b24b0013f028f2b2c89b7515ae4ed11c3
Soul-Remix <soulremix-dev@pm.me>
2022-10-16 22:28:11 +0300

    add seam removal methods and refactor seam finding methods

--- Algs4/Algorithms, Part II/Week 2/SeamCarver/SeamCarver.java
+++ Algs4/Algorithms, Part II/Week 2/SeamCarver/SeamCarver.java
@@ -1,233 +1,244 @@
-import edu.princeton.cs.algs4.BellmanFordSP;
-import edu.princeton.cs.algs4.DirectedEdge;
-import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.Picture;
 
 import java.awt.Color;
 
 public class SeamCarver {
-    private Picture picture;
     private int width;
     private int height;
-    private int[][] pixels;
+    private double[][] energyMatrix;
+    private int[][] colorMatrix;
+    private boolean transposed;
 
-    // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
-        this.picture = new Picture(picture);
-        width = this.picture.width();
-        height = this.picture.height();
+        if (picture == null) {
+            throw new IllegalArgumentException();
+        }
+        width = picture.width();
+        height = picture.height();
 
-        pixels = new int[height][width];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                pixels[i][j] = picture.get(j, i).getRGB();
+        buildM(picture);
+
+        transposed = false;
+    }
+
+    private void buildM(Picture p) {
+        energyMatrix = new double[width][height];
+        colorMatrix = new int[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                // caution: this is a transposed matrix
+                Color c = p.get(i, j);
+                colorMatrix[i][j] = c.getRed() + c.getGreen() * 256 + c.getBlue() * 65536;
+            }
+        }
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                energyMatrix[i][j] = calculateEnergy(i, j);
             }
         }
     }
 
-    // current picture
-    public Picture picture() {
-        return picture;
-    }
-
-    // width of current picture
-    public int width() {
-        return width;
-    }
-
-    // height of current picture
-    public int height() {
-        return height;
-    }
-
-    // energy of pixel at column x and row y
-    public double energy(int x, int y) {
-        validateWidth(x);
-        validateHeight(y);
-        if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) {
+    private double calculateEnergy(int i, int j) {
+        if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
             return 1000;
         }
-
-        Color color1, color2;
-
-        color1 = new Color(pixels[y - 1][x]);
-        color2 = new Color(pixels[y + 1][x]);
-        double squareY = Math.pow(color1.getRed() - color2.getRed(), 2) + Math.pow(
-                color1.getGreen() - color2.getGreen(), 2) + Math.pow(
-                color1.getBlue() - color2.getBlue(), 2);
-        color1 = new Color(pixels[y][x - 1]);
-        color2 = new Color(pixels[y][x + 1]);
-        double squareX = Math.pow(color1.getRed() - color2.getRed(), 2) + Math.pow(
-                color1.getGreen() - color2.getGreen(), 2) + Math.pow(
-                color1.getBlue() - color2.getBlue(), 2);
-        return Math.sqrt(squareX + squareY);
+        else {
+            double deltax = difference(colorMatrix[i - 1][j], colorMatrix[i + 1][j]);
+            double deltay = difference(colorMatrix[i][j - 1], colorMatrix[i][j + 1]);
+            return Math.sqrt(deltax + deltay);
+        }
     }
 
-    // sequence of indices for horizontal seam
-    public int[] findHorizontalSeam() {
-        int[][] idx = new int[height()][width()];
-        int numId = 0;
-        int curr = 0;
-        while (curr < width()) {
-            for (int i = 0; i < height(); i++) {
-                idx[i][curr] = numId;
-                numId++;
-            }
-            curr++;
-        }
-
-        EdgeWeightedDigraph digraph = new EdgeWeightedDigraph(width() * height() + 1);
-
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                if (j + 1 < width()) {
-                    digraph.addEdge(new DirectedEdge(idx[i][j], idx[i][j + 1], energy(j, i)));
-                    if (i + 1 < height()) {
-                        digraph.addEdge(
-                                new DirectedEdge(idx[i][j], idx[i + 1][j + 1], energy(j, i)));
-                    }
-                    if (i - 1 > 0) {
-                        digraph.addEdge(
-                                new DirectedEdge(idx[i][j], idx[i - 1][j + 1], energy(j, i)));
-                    }
-                }
-                else {
-                    digraph.addEdge(new DirectedEdge(idx[i][j], width() * height(), energy(j, i)));
-                }
-            }
-        }
-
-        double min = Double.POSITIVE_INFINITY;
-        Iterable<DirectedEdge> pathIter = null;
-
-
-        for (int i = 0; i < height(); i++) {
-            BellmanFordSP sp = new BellmanFordSP(digraph, i);
-            double dist = sp.distTo(width() * height());
-            if (dist < min) {
-                min = dist;
-                pathIter = sp.pathTo(width() * height());
-            }
-        }
-
-        int[] path = new int[width()];
-        int id = 0;
-
-        for (DirectedEdge edge : pathIter) {
-            int from = edge.from();
-            for (int i = 0; i < height(); i++) {
-                if (idx[i][id] == from) {
-                    path[id] = i;
-                }
-            }
-            id++;
-        }
-        return path;
+    private double difference(int c1, int c2) {
+        int[] rgb1 = rgb(c1);
+        int[] rgb2 = rgb(c2);
+        double dr = Math.pow((double) rgb1[0] - rgb2[0], 2);
+        double dg = Math.pow((double) rgb1[1] - rgb2[1], 2);
+        double db = Math.pow((double) rgb1[2] - rgb2[2], 2);
+        return dr + dg + db;
     }
 
-    // sequence of indices for vertical seam
-    public int[] findVerticalSeam() {
-        int[][] idx = new int[height()][width()];
-        int numId = 0;
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                idx[i][j] = numId;
-                numId++;
-            }
-        }
+    private int[] rgb(int c) {
+        int[] rgb = new int[3];
+        rgb[2] = c / 65536;
+        c %= 65536;
+        rgb[1] = c / 256;
+        rgb[0] = c % 256;
+        return rgb;
+    }
 
-        EdgeWeightedDigraph digraph = new EdgeWeightedDigraph(width() * height() + 1);
-
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                if (i + 1 < height()) {
-                    digraph.addEdge(new DirectedEdge(idx[i][j], idx[i + 1][j], energy(j, i)));
-                    if (j + 1 < width()) {
-                        digraph.addEdge(
-                                new DirectedEdge(idx[i][j], idx[i + 1][j + 1], energy(j, i)));
-                    }
-                    if (j - 1 > 0) {
-                        digraph.addEdge(
-                                new DirectedEdge(idx[i][j], idx[i + 1][j - 1], energy(j, i)));
-                    }
-                }
-                else {
-                    digraph.addEdge(new DirectedEdge(idx[i][j], width() * height(), energy(j, i)));
-                }
-            }
-        }
-
-        double min = Double.POSITIVE_INFINITY;
-        Iterable<DirectedEdge> pathIter = null;
-
+    private void transpose() {
+        int t = width;
+        width = height;
+        height = t;
+        double[][] energyT = new double[width][height];
+        int[][] colorT = new int[width][height];
         for (int i = 0; i < width; i++) {
-            BellmanFordSP sp = new BellmanFordSP(digraph, i);
-            double dist = sp.distTo(width * height);
-            if (dist < min) {
-                min = dist;
-                pathIter = sp.pathTo(width * height);
+            for (int j = 0; j < height; j++) {
+                energyT[i][j] = energyMatrix[j][i];
+                colorT[i][j] = colorMatrix[j][i];
             }
         }
+        energyMatrix = energyT;
+        colorMatrix = colorT;
 
-        int[] path = new int[height()];
-        int id = 0;
+        transposed = !transposed;
+    }
 
-        for (DirectedEdge edge : pathIter) {
-            int from = edge.from();
-            for (int i = 0; i < width(); i++) {
-                if (idx[id][i] == from) {
-                    path[id] = i;
+    public Picture picture() {
+        if (transposed) {
+            transpose();
+        }
+        Picture p = new Picture(width, height);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int[] rgb = rgb(colorMatrix[i][j]);
+                p.set(i, j, new Color(rgb[0], rgb[1], rgb[2]));
+            }
+        }
+        return p;
+    }
+
+    public int width() {
+        if (transposed) {
+            return height;
+        }
+        else {
+            return width;
+        }
+    }
+
+    public int height() {
+        if (transposed) {
+            return width;
+        }
+        else {
+            return height;
+        }
+    }
+
+    public double energy(int x, int y) {
+        if (x < 0 || width() <= x || y < 0 || height() <= y) {
+            throw new IllegalArgumentException();
+        }
+        if (transposed) {
+            return energyMatrix[y][x];
+        }
+        else {
+            return energyMatrix[x][y];
+        }
+    }
+
+    public int[] findHorizontalSeam() {
+        if (transposed) {
+            transpose();
+        }
+        return findHSeam();
+    }
+
+    private int[] findHSeam() {
+        int[] result = new int[width];
+        if (2 < height) {
+            double[][] distTo = buildDistTo();
+            int[][] edgeTo = new int[width][height];
+            for (int i = 0; i < width - 1; i++) {
+                for (int j = 1; j < height - 1; j++) {
+                    relax(i, j, distTo, edgeTo);
                 }
             }
-            id++;
+            int ry = 1;
+            for (int j = 1; j < height - 1; j++) {
+                if (distTo[width - 1][j] < distTo[width - 1][ry]) {
+                    ry = j;
+                }
+            }
+
+            for (int i = width - 1; 0 <= i; i--) {
+                result[i] = ry;
+                ry = edgeTo[i][ry];
+            }
         }
-        return path;
+
+        return result;
     }
 
-    // remove horizontal seam from current picture
-    public void removeHorizontalSeam(int[] seam) {
-        validateHorizontalSeam(seam);
-    }
-
-    // remove vertical seam from current picture
-    public void removeVerticalSeam(int[] seam) {
-        validateVerticalSeam(seam);
-    }
-
-    private void validateWidth(int num) {
-        if (num < 0 || num > width() - 1) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateHeight(int num) {
-        if (num < 0 || num > height() - 1) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateVerticalSeam(int[] seam) {
-        if (seam.length != height() || width() <= 1) {
-            throw new IllegalArgumentException();
-        }
-        for (int i = 0; i < seam.length; i++) {
-            validateHeight(seam[i]);
-            if (i < seam.length - 1 && seam[i + 1] > (seam[i] + 1)) {
-                throw new IllegalArgumentException();
+    private void relax(int x, int y, double[][] distTo, int[][] edgeTo) {
+        for (int j = y - 1; j <= y + 1; j++) {
+            double d = distTo[x][y] + energyMatrix[x + 1][j];
+            if (d < distTo[x + 1][j]) {
+                distTo[x + 1][j] = d;
+                edgeTo[x + 1][j] = y;
             }
         }
     }
 
-    private void validateHorizontalSeam(int[] seam) {
-        if (seam.length != width() || height() <= 1) {
-            throw new IllegalArgumentException();
+    public int[] findVerticalSeam() {
+        if (!transposed) {
+            transpose();
         }
-        for (int i = 0; i < seam.length; i++) {
-            validateWidth(seam[i]);
-            if (i < seam.length - 1 && seam[i + 1] > (seam[i] + 1)) {
-                throw new IllegalArgumentException();
+        return findHSeam();
+    }
+
+    private double[][] buildDistTo() {
+        double[][] distTo = new double[width][height];
+        for (int i = 1; i < width; i++) {
+            for (int j = 1; j < height; j++) {
+                distTo[i][j] = Double.POSITIVE_INFINITY;
             }
         }
+        return distTo;
+    }
+
+    private void validate(int[] seam) {
+        if (seam == null) {
+            throw new IllegalArgumentException();
+        }
+        if (seam.length != width || height < 2) {
+            throw new IllegalArgumentException();
+        }
+        int pre = seam[0];
+        for (int s : seam) {
+            if (s < 0 || height <= s || 1 < Math.abs(s - pre)) {
+                throw new IllegalArgumentException();
+            }
+            else {
+                pre = s;
+            }
+        }
+    }
+
+
+    public void removeHorizontalSeam(int[] seam) {  // remove horizontal seam from current picture
+        if (transposed) {
+            transpose();
+        }
+        removeHSeam(seam);
+    }
+
+    private void removeHSeam(int[] seam) {
+        validate(seam);
+        height--;
+        for (int i = 0; i < width; i++) {
+            System.arraycopy(energyMatrix[i], seam[i] + 1, energyMatrix[i], seam[i],
+                             height - seam[i]);
+            System.arraycopy(colorMatrix[i], seam[i] + 1, colorMatrix[i], seam[i],
+                             height - seam[i]);
+        }
+        for (int i = 1; i < width - 1; i++) {
+            if (0 < seam[i]) {
+                energyMatrix[i][seam[i] - 1] = calculateEnergy(i, seam[i] - 1);
+            }
+            if (seam[i] < height) {
+                energyMatrix[i][seam[i]] = calculateEnergy(i, seam[i]);
+            }
+        }
+    }
+
+    public void removeVerticalSeam(int[] seam) {    // remove vertical seam from current picture
+        if (!transposed) {
+            transpose();
+        }
+        removeHSeam(seam);
     }
 
     //  unit testing (optional)
@@ -235,8 +246,13 @@ public class SeamCarver {
         Picture picture = new Picture(args[0]);
         SeamCarver carver = new SeamCarver(picture);
 
-        System.out.println(carver.energy(0, 3));
-        System.out.println(carver.findHorizontalSeam());
+        System.out.println(carver.energy(3, 3));
+        carver.removeVerticalSeam(carver.findVerticalSeam());
+        carver.removeHorizontalSeam(carver.findHorizontalSeam());
+        Picture pic = carver.picture();
+        System.out.println(carver.width() + "    " + pic.width());
+        System.out.println(carver.height() + "    " + pic.height());
+        carver.removeHorizontalSeam(carver.findHorizontalSeam());
     }
 
 }
