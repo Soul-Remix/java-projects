ec33eb64c588978c707da81cdea7b6e3ae67b65e
Soul-Remix <soulremix-dev@pm.me>
2022-10-19 09:21:05 +0300

    add get all valid words method
    create a directed graph, added edges for each vertex and it's neighbours, use dfs to search for words

--- Algs4/Algorithms, Part II/Week 4/Boggle/BoggleSolver.java
+++ Algs4/Algorithms, Part II/Week 4/Boggle/BoggleSolver.java
@@ -1,5 +1,9 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.TST;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BoggleSolver {
@@ -13,6 +17,90 @@ public class BoggleSolver {
         for (int i = 0; i < dictionary.length; i++) {
             dict.put(dictionary[i], i);
         }
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        int rows = board.rows();
+        int cols = board.cols();
+        verticesCells = new HashMap<>();
+
+        int[][] vertices = new int[rows][cols];
+        int id = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                vertices[i][j] = id;
+                verticesCells.put(id, new Integer[] { i, j });
+                id++;
+            }
+        }
+
+        Digraph digraph = new Digraph(cols * rows);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int vertixId = vertices[i][j];
+                if (i > 0) {
+                    digraph.addEdge(vertixId, vertices[i - 1][j]);
+                }
+                if (j > 0) {
+                    digraph.addEdge(vertixId, vertices[i][j - 1]);
+                }
+                if (i < rows - 1) {
+                    digraph.addEdge(vertixId, vertices[i + 1][j]);
+                }
+                if (j < cols - 1) {
+                    digraph.addEdge(vertixId, vertices[i][j + 1]);
+                }
+                if (i > 0 && j > 0) {
+                    digraph.addEdge(vertixId, vertices[i - 1][j - 1]);
+                }
+                if (i > 0 && j < cols - 1) {
+                    digraph.addEdge(vertixId, vertices[i - 1][j + 1]);
+                }
+                if (i < rows - 1 && j < cols - 1) {
+                    digraph.addEdge(vertixId, vertices[i + 1][j + 1]);
+                }
+                if (i < rows - 1 && j > 0) {
+                    digraph.addEdge(vertixId, vertices[i + 1][j - 1]);
+                }
+            }
+        }
+
+        ArrayList<String> words = new ArrayList<>();
+        boolean[] marked = new boolean[digraph.V()];
+        for (int i = 0; i < digraph.V(); i++) {
+            String word = "";
+            dfs(digraph, i, marked, word, board, words);
+        }
+
+        return words;
+    }
+
+    private void dfs(Digraph g, int v, boolean[] marked, String word, BoggleBoard board,
+                     ArrayList<String> words) {
+        Integer[] cell = verticesCells.get(v);
+        int i = cell[0];
+        int j = cell[1];
+        char letter = board.getLetter(i, j);
+        if (letter == 'Q') {
+            word += "QU";
+        }
+        else {
+            word += letter;
+        }
+        if (!dict.keysWithPrefix(word).iterator().hasNext()) {
+            return;
+        }
+        marked[v] = true;
+        if (word.length() >= 3 && dict.contains(word) && !words.contains(word)) {
+            words.add(word);
+        }
+        for (int w : g.adj(v)) {
+            if (!marked[w]) {
+                dfs(g, w, marked, word, board, words);
+            }
+        }
+        marked[v] = false;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
@@ -43,16 +131,16 @@ public class BoggleSolver {
     }
 
     public static void main(String[] args) {
-        // In in = new In(args[0]);
-        // String[] dictionary = in.readAllStrings();
-        // BoggleSolver solver = new BoggleSolver(dictionary);
-        // BoggleBoard board = new BoggleBoard(args[1]);
-        // int score = 0;
-        // for (String word : solver.getAllValidWords(board)) {
-        //     StdOut.println(word);
-        //     score += solver.scoreOf(word);
-        // }
-        // StdOut.println("Score = " + score);
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
 
 
         // Performance test
